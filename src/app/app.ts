3542ea4a6c10147934d15f0dda20b3895c92cbaa
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-09-06 11:34:22 -0300

    esqueleto do projeto feito

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { TelaBatalha } from "./tela-batalha/tela-batalha";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TelaBatalha],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/tela-batalha/tela-batalha.ts
+++ src/app/tela-batalha/tela-batalha.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './tela-batalha.css'
 })
 export class TelaBatalha {
+// --- Nossos Dados Falsos (Mock) ---
+  jogador = {
+    nome: 'V',
+    hp: 100,
+    ep: 20,
+    stims: 2
+  };
+
+  inimigo = {
+    nome: 'Drone de Segurança',
+    hp: 50
+  };
+
+  mensagem = 'O que você vai fazer?';
+  // --- Fim dos Dados Falsos ---
+
+  constructor() { }
 
 }
