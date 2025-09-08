c33b05a849ed004ded3506e868bb42a857a6f197
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-09-08 19:16:13 -0300

    botoes funcionando.

--- src/app/tela-batalha/tela-batalha.ts
+++ src/app/tela-batalha/tela-batalha.ts
@@ -20,6 +20,27 @@ export class TelaBatalha {
     hp: 50
   };
 
+  public ataqueFisico(): void {
+    console.log('Ataque Físico Funcionando');
+    this.inimigo.hp -= 10;
+  }
+
+  public ataqueCibernetico(): void {
+    console.log('Ataque Cibernético Funcionando');
+    this.inimigo.hp -= 15;
+    this.jogador.ep -= 5;
+  }
+
+  public usarStim(): void {
+    if (this.jogador.stims <= 0){
+      return;
+    }
+    console.log('Usar Stim Funcionando');
+    this.jogador.hp += 20;
+    this.jogador.stims -= 1;
+
+  }
+
   mensagem = 'O que você vai fazer?';
   // --- Fim dos Dados Falsos ---
 
